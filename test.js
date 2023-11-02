a491d459b0adffbb704cbc8f15bd93a8997f592f
unknown <adetutu.francisb@gmail.com>
2023-11-01 21:01:00 -0400

    JSlessons

---
+++ test.js
@@ -0,0 +1,84 @@
+// alert('Hello World');
+// var myName='Adetutu'
+// document.write(myName)
+
+// console.log(myName)
+// var dogsHaveFur=false
+
+// if (dogsHaveFur) {/* this is called a code block anything between these two curly braces is the code block that we're going to execute
+// */
+//   document.write("yes, dogs have fur")  
+// }
+// else {
+//     document.write('Someone shaved this dog :( ')
+// }
+// var myAge=22
+
+// if(myAge >30){
+//     document.write('warning, midlife crisis')  
+// }
+// else if(myAge > 20){
+//     document.write('you are in your prime')
+// }
+// else if (myAge>10) {
+    
+// }
+// else{
+//     document.write('you are a baby')
+
+
+// }
+
+// var personAge = 35; 
+// if (personAge >= 17 || personAge <= 30){
+
+//     document.write("You can go to the concert!")
+// } else {
+//     document.write("You need to stay home :p");
+// }
+// var personAge = 5;
+
+
+// while(personAge < 10){
+//  console.log("You are less than 10!")
+// personAge++;
+// }
+
+
+// // document.write('You are over 10!')
+// for (personAge = 5; personAge < 10; personAge++);
+// document.write('you are less than 10')
+// var links = document.getElementsByTagName('a')
+// for(i = 1; i <= links.length; i++) {
+//     console.log('this is link number' + i)
+// }
+// document.write('All Links have been looped')
+// for (i = 0; i < 10; i++){
+//     //loop body
+//     console.log(i);
+//     if(i === 7) {
+//         console.log('the loop is broken');
+//         break;
+//         }
+//         }
+        
+for (i = 0; i < 10; i++) {
+    
+    if (i === 5 || i === 3) {
+        
+        continue;
+    }
+
+
+    console.log(i);
+
+
+    if(i === 7) {
+        console.log("The Loop is broken")
+        break; 
+        
+                }
+   
+                        }
+
+        
